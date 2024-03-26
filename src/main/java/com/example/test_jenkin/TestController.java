3c048a6861a813fbdba0b2e3f3d45238cf5c1f5d
minhnt <minhnt@biplus.com.vn>
2024-03-26 14:47:42 +0700

    commit

---
+++ src/main/java/com/example/test_jenkin/TestController.java
@@ -0,0 +1,13 @@
+package com.example.test_jenkin;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/testtt")
+    public ResponseEntity register() {
+        return ResponseEntity.ok("chay dc roi");
+    }
+}
---
+++ src/main/java/com/example/test_jenkin/TestJenkinApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_jenkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJenkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJenkinApplication.class, args);
+	}
+
+}

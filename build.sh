3d98dcc492c79d366ac44f34096ce705d1151d76
minhnt <minhnt@biplus.com.vn>
2024-03-28 17:15:29 +0700

    commit

--- build.sh
+++ build.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-file_path="/home/minh/tesstJenkins"
-
-cd $file_path
-if [ ! -e "${file_path}/testtam" ]; then
-    echo "Tệp ${file_path}/testtam không tồn tại."
-    git clone https://github.com/minh2561/testtam.git
-    echo 'cấp quyền thực thi'
-    chmod +x gradlew
-fi
 echo 'git pull'
 git pull
 echo 'gradlew clean'
---
+++ login.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/expect -f
+
+set timeout -1
+spawn ssh minh@45.76.146.134
+expect "password:"
+send -- "conheohong\r"
+expect "$ "
+send -- "cd /home/minh/tesstJenkins/testtam\r"
+expect "$ "
+send -- "chmod +x build\r"
+expect "$ "
+send -- "./build.sh\r"
+send -- "exit\r"
+expect eof
